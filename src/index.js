75b97f0157b8e65fd926e70960b7a263fe36a310
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-26 00:49:38 +0200

    Import load page to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './styles.css';
 import restaurantLogo from './assets/restaurant.png';
+import { loadPage } from './initial-page-load';
 
 const image = document.createElement('img');
 image.src = restaurantLogo;
 
 document.body.appendChild(image);
 
-console.log("The Restaurant is open!");
+loadPage();
