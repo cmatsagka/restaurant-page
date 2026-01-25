f5f8e5b0bd660c804becae07d67a121f10eda487
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-26 01:19:00 +0200

    Add query selectors and event listeners for buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import './styles.css';
 import { loadPage } from './initial-page-load.js';
 
+const content = document.querySelector('#content');
 
+const homeBtn = document.querySelector('#home-btn');
+const menuBtn = document.querySelector('#menu-btn');
+const aboutBtn = document.querySelector('#about-btn');
+const contactBtn = document.querySelector('#contact-btn');
+
+homeBtn.addEventListener('click', () => {
+    console.log('Home clicked');
+});
+
+menuBtn.addEventListener('click', () => {
+    console.log('Menu clicked');
+});
+
+aboutBtn.addEventListener('click', () => {
+    console.log('about clicked');
+});
+
+contactBtn.addEventListener('click', () => {
+    console.log('contact clicked');
+});
 
 loadPage();
