345289bac4f7f628f02f8e61d2ea48982a685e2d
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-26 00:48:03 +0200

    Create and export function for initial load page

---
+++ src/initial-page-load.js
@@ -0,0 +1,11 @@
+export function loadPage() {
+    const content = document.querySelector('#content');
+
+    const headline = document.createElement('h1');
+    headline.textContent = "Zoumi";
+    content.appendChild(headline);
+
+    const description = document.createElement('p');
+    description.textContent = "A modern bistro based on quality local products.";
+    content.appendChild(description);
+}
