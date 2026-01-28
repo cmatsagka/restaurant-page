359afafb250c73e6f537868a4532c91028103d71
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-28 16:09:59 +0200

    Add classes to menu page

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -6,14 +6,17 @@ export function loadPage() {
 
     const headline = document.createElement('h1');
     headline.textContent = "Zoumi";
+    headline.classList.add('home-title');
     content.appendChild(headline);
 
     const description = document.createElement('p');
     description.textContent = "A modern bistro based on quality local products.";
+    description.classList.add('home-description');
     content.appendChild(description);
 
     const image = document.createElement('img');
     image.src = restaurantLogo;
     image.alt = "Zoumi Bistro logo";
+    image.classList.add('home-logo');
     content.appendChild(image);
 }
--- src/menu.js
+++ src/menu.js
@@ -12,31 +12,38 @@ export function loadMenu() {
 
     const restaurantName = document.createElement('h2');
     restaurantName.textContent = "Zoumi";
+    restaurantName.classList.add('restaurant-name');
     content.appendChild(restaurantName);
 
     const image = document.createElement('img');
     image.src = restaurantLogo;
     image.alt = "Zoumi Bistro logo";
+    image.classList.add('logo-small');
     content.appendChild(image);
 
     const headline = document.createElement('h1');
     headline.textContent = "Menu";
+    headline.classList.add('page-title');
     content.appendChild(headline);
 
     const description = document.createElement('p');
     description.textContent = "Menu items list";
+    description.classList.add('page-descr');
     content.appendChild(description);
 
     const list = document.createElement('ul');
+    list.classList.add('menu');
     
     for (let i = 0; i < menuItems.length; i++) {
         const listItem = document.createElement('li');
+        listItem.classList.add('menu-item');
 
         const itemName = document.createElement('h3');
         itemName.textContent = menuItems[i].name;
 
         const itemPrice = document.createElement('span');
         itemPrice.textContent = menuItems[i].price;
+        itemPrice.classList.add('item-price');
 
         const itemDescription= document.createElement('p');
         itemDescription.textContent = menuItems[i].description;
