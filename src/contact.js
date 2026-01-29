81bdbde3f0a15679a59f7200ae604fed5debfac7
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-29 20:02:44 +0200

    Add logoName div to contact page

--- src/contact.js
+++ src/contact.js
@@ -4,19 +4,23 @@ export function loadContact() {
 	const content = document.querySelector('#content');
 	content.classList.add('contact-page');
 
+	const logoName = document.createElement('div');
+	logoName.classList.add('logo-name');
+	content.appendChild(logoName);
+
 	const restaurantName = document.createElement('h2');
 	restaurantName.textContent = 'Zoumi';
 	restaurantName.classList.add('restaurant-name');
-	content.appendChild(restaurantName);
+	logoName.appendChild(restaurantName);
 
 	const image = document.createElement('img');
 	image.src = restaurantLogo;
 	image.alt = 'Zoumi Bistro logo';
 	image.classList.add('logo-small');
-	content.appendChild(image);
+	logoName.appendChild(image);
 
 	const headline = document.createElement('h1');
-	headline.textContent = 'Contact';
+	headline.textContent = 'Our story';
 	headline.classList.add('page-title');
 	content.appendChild(headline);
 
