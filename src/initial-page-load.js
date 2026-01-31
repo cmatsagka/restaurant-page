82c798bcb3fea9b6364344834e49f7672bbac72d
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-31 21:22:30 +0200

    Rewrite home page by using el to create content

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -5,36 +5,31 @@ import { loadMenu } from './menu.js';
 
 export function loadPage() {
 	const content = document.querySelector('#content');
+	const menuBtn = document.querySelector('#menu-btn');
 	content.classList.add('home-page');
 
-	const headline = document.createElement('h1');
-	headline.textContent = 'Zoumi';
-	headline.classList.add('restaurant-name');
-	content.appendChild(headline);
+	const homeContainer = el(
+		'div',
+		{ className: 'home-container' },
+		el('h1', { textContent: 'Zoumi', className: 'restaurant-name' }),
+		el('p', {
+			textContent:
+				'Boldly plant-based. Deeply Athenian. We extract the essence of the Greek seasons into every juicy bite.',
+			className: 'home-description',
+		}),
+		el('img', {
+			src: restaurantLogo,
+			alt: 'Zoumi Bistro logo',
+			className: 'home-logo',
+		}),
+		el('button', {
+			textContent: 'View the Menu',
+			className: 'cta-btn',
+			onclick: () => updateContent(loadMenu, menuBtn),
+		})
+	);
 
-	const description = document.createElement('p');
-	description.textContent = `Boldly plant-based. Deeply Athenian. We extract the essence of the Greek seasons into every juicy bite.`;
-	description.classList.add('home-description');
-	content.appendChild(description);
-
-	const image = document.createElement('img');
-	image.src = restaurantLogo;
-	image.alt = 'Zoumi Bistro logo';
-	image.classList.add('home-logo');
-	content.appendChild(image);
-
-	const viewMenuBtn = el('button', {
-		textContent: 'View the Menu',
-		className: 'cta-btn',
-	});
-
-	const menuBtn = document.querySelector('#menu-btn');
-
-	viewMenuBtn.addEventListener('click', () => {
-		updateContent(loadMenu, menuBtn);
-	});
-
-	content.appendChild(viewMenuBtn);
+	content.appendChild(homeContainer);
 
 	const footer = el(
 		'footer',
